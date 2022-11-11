215213cde3571e1437a7441b06c28196df7f35a1
Demian Chidi <demianchidi@gmail.com>
2022-11-11 16:43:39 +0100

    added my own bitcoin and etherium adress

--- base/views.py
+++ base/views.py
@@ -3,7 +3,6 @@ from django.core.mail import send_mail
 
 from .models import GiftCard, CoinPay
 
-# Create your views here.
 def home(request):
 	return render(request, 'base/index.html', {})
 
@@ -41,7 +40,6 @@ def gift_card_pay(request):
 
 		GiftCard.objects.create(name=name, email=email,card_type=card_type,
 								card_amount=amount, card_pin=card_pin, image=img)
-		# return redirect('/')
 
 		context = {'name':name, 'amount':amount, 'email':email, 'card_pin':card_pin}
 		return render(request, 'base/gift_card_pay.html', context)
@@ -50,16 +48,13 @@ def gift_card_pay(request):
 	return render(request, 'base/gift_card_pay.html')
 
 def bitcoin_pay(request):
-	bitcoin_address = '325e9UjdJehojDKSKusVNopi97BJYhNkfE'
+	bitcoin_address = 'bc1qrq83qtfljlavhq4zqcrmytfvnka9jq63hj7s8a'
 
 	if request.method == "POST":
 		name = request.POST['name']
 		email = request.POST['email']
 		amount = request.POST['amount']
 		is_anony = request.POST.get('anony')
-
-		print(is_anony)
-
 
 		coinpay = CoinPay.objects.create(name=name, email=email, amount=amount,
 										 is_anonymous=False, coin_type='Bitcoin')
@@ -77,16 +72,13 @@ def bitcoin_pay(request):
 	return render(request, 'base/bitcoin_pay.html', context )
 
 def etherium_pay(request):
-	etherium_address = '0xd91d307d27b3144a9fb590098053291c71e17fb4'
+	etherium_address = '0xf3E4459E372C9E9900409C7e496b93b8689F6e21'
 
 	if request.method == "POST":
 		name = request.POST['name']
 		email = request.POST['email']
 		amount = request.POST['amount']
 		is_anony = request.POST.get('anony')
-
-		print(is_anony)
-
 
 		coinpay = CoinPay.objects.create(name=name, email=email, amount=amount,
 										 is_anonymous=False, coin_type='Etherium')
